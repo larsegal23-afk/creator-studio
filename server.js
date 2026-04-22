e4118fe5483a119ae27f0d99a00dfc5481be77c6
larsegal23-afk <larsegal23@googlemail.com>
2026-04-22 12:52:11 +0200

    Fix CORS origin setting

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const PORT = process.env.PORT || 3000
 // CORS - KOMPLETT OFFEN
 // ================================
 app.use(cors({
-  origin: "*",
+  origin: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"],
   credentials: true
